143c1e4eab4eeadcea3e1d8739386de13245cfb8
Justin Chiou <justin@thrivera.io>
2018-05-12 11:23:25 -0400

    scaffold for the navigation components

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,22 @@
 <template>
-  <section class="container">
+  <section>
     <div>
-      <app-logo/>
-      <h1 class="title">{{ title }}</h1>
-      <h2 class="subtitle">Static site redesign of ballroomeast.com</h2>
-      <div class="links">
+      <h1>{{ title }}</h1>
+      <h2>Static site redesign of ballroomeast.com</h2>
+      <div>
         <a
           href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
+          target="_blank">Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+          target="_blank">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '@/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  },
   data () {
     return {
       title: 'Ballroom East'
@@ -32,34 +24,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
