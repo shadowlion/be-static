21ba1640cff3f541483697195481bc543ba37baf
Justin Chiou <justin@thrivera.io>
2018-05-15 23:03:09 -0400

    init template for NavBar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
+    <navBar />
     <nuxt />
     <footerSection />
   </div>
 </template>
 
 <script>
+import navBar from '@/components/nav/NavBar'
 import footerSection from '@/components/Footer'
 
 export default {
   components: {
-    footerSection
+    navBar, footerSection
   }
 }
 </script>
