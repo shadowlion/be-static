e7b178a4e010f72a1871a002a251fc696fc5073a
Justin Chiou <justin@thrivera.io>
2018-05-17 22:10:27 -0400

    add bulma panels to organize studio info

--- pages/studio.vue
+++ pages/studio.vue
@@ -1,24 +1,38 @@
 <template>
-  <section>
-    <h3>A New Concept in Dance Instruction</h3>
-    <p>Ballroom East was founded with a rebellious spirit and a lofty objective: to create an environment of high level dancing in a non franchise studio with no contractual agreements.</p>
-    <p>We had the dream of a beautiful studio filled with great dancers of all ages and ethnicities. To have an environment that would inspire everyone to be great dancers.</p>
-
-    <h3>Lessons With A Purpose</h3>
-    <p>All of our lessons have a purpose. To have fun and at the same time transform the dancing into something great, whether for social dancing or competitive dancing.</p>
-
-    <h3>Why did we name our company Ballroom East?</h3>
-    <p>When we purchased the property in 1986 the business was named Ballet East. We decided to keep the original name but change it to Ballroom East. We thought it appropriate considering the location is in the east end of town in the heart of St Matthews, Louisville, KY.</p>
-    <p>We were encouraged and inspired by a very great man, Dr. Alan Thomas. With his vision and passion for dance Ballroom East became a reality.</p>
-
-    <article class="message is-info">
-  <div class="message-header">
-    <p>Info</p>
-    <button class="delete" aria-label="delete"></button>
-  </div>
-  <div class="message-body">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-  </div>
-</article>
+  <section class="container">
+    <div class="columns">
+      <div class="column">
+        <article class="message is-info">
+          <div class="message-header">
+            <p>A New Concept in Dance Instruction</p>
+          </div>
+          <div class="message-body">
+            Ballroom East was founded with a rebellious spirit and a lofty objective: to create an environment of high level dancing in a non franchise studio with no contractual agreements.<br><br>
+            We had the dream of a beautiful studio filled with great dancers of all ages and ethnicities. To have an environment that would inspire everyone to be great dancers.
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message is-info">
+          <div class="message-header">
+            <p>Lessons With A Purpose</p>
+          </div>
+          <div class="message-body">
+            All of our lessons have a purpose. To have fun and at the same time transform the dancing into something great, whether for social dancing or competitive dancing.
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message is-info">
+          <div class="message-header">
+            <p>Why name our company Ballroom East?</p>
+          </div>
+          <div class="message-body">
+            When we purchased the property in 1986 the business was named Ballet East. We decided to keep the original name but change it to Ballroom East. We thought it appropriate considering the location is in the east end of town in the heart of St Matthews, Louisville, KY.<br><br>
+            We were encouraged and inspired by a very great man, Dr. Alan Thomas. With his vision and passion for dance, Ballroom East became a reality.
+          </div>
+        </article>
+      </div>
+    </div>
   </section>
 </template>
