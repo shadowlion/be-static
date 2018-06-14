eff2c6475544cef2f0b76ebebe1ce8163693ae51
Justin <chiou.kai@gmail.com>
2018-06-13 22:03:37 -0400

    refactor where the navbar is placed

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,17 +1,124 @@
 <template>
-  <div class="mobile-menu-div w-hidden-main">
-    <div class="w-hidden-main w-row">
-      <div class="mobile-nav-col w-col w-col-2 w-col-small-2 w-col-tiny-2"><a href="#" data-ix="mobile-menu-activation" class="mobile-menu-link">Menu</a></div>
-      <div class="w-col w-col-8 w-col-small-8 w-col-tiny-8"><a href="index.html" class="logo-link w-inline-block w--current"><img src="images/beLogo.png" width="600"></a></div>
-      <div class="mobile-nav-col w-col w-col-2 w-col-small-2 w-col-tiny-2"><a href="#" class="mobile-cart-link">Cart</a></div>
+  <div>
+    <div class="mobile-menu-div w-hidden-main">
+      <div class="w-hidden-main w-row">
+        <div class="mobile-nav-col w-col w-col-2 w-col-small-2 w-col-tiny-2"><a href="#" data-ix="mobile-menu-activation" class="mobile-menu-link">Menu</a></div>
+        <div class="w-col w-col-8 w-col-small-8 w-col-tiny-8"><a href="index.html" class="logo-link w-inline-block w--current"><img src="images/beLogo.png" width="600"></a></div>
+        <div class="mobile-nav-col w-col w-col-2 w-col-small-2 w-col-tiny-2"><a href="#" class="mobile-cart-link">Cart</a></div>
+      </div>
+      <div data-ix="hidden-mobile-menu" class="mobile-nav-dropdown">
+        <div class="w-row">
+          <div class="w-col w-col-2 w-col-small-2 w-col-tiny-2"></div>
+          <div class="w-col w-col-8 w-col-small-8 w-col-tiny-8"><a href="index.html" class="logo-link w-inline-block w--current"><img src="images/beLogo.png" width="724"></a></div>
+          <div class="mobile-nav-col dropdown w-col w-col-2 w-col-small-2 w-col-tiny-2"><a href="#" data-ix="close-mobile-menu" class="close-button"><strong data-new-link="true">X</strong></a></div>
+        </div><a href="lessons/lessons-sitemap.html" class="dropdown-banner-link w-hidden-small w-inline-block"><img src="images/brighterBanner01.png" width="580" class="banner-img"></a>
+        <div class="mobile-menu-link-div"><a href="lessons/lessons-sitemap.html" class="mobile-nav-link">Lessons</a><a href="services/services-sitemap.html" class="mobile-nav-link">Services</a><a href="about-us/about-us-sitemap.html" class="mobile-nav-link">About Us</a><a href="calendar.html" class="mobile-nav-link">Calendar</a><a href="gift-certificates.html" class="mobile-nav-link">Gift Certificates</a><a href="visit-us.html" class="mobile-nav-link">Visit Us</a><a href="contact.html" class="mobile-nav-link">Contact Us</a></div>
+      </div>
     </div>
-    <div data-ix="hidden-mobile-menu" class="mobile-nav-dropdown">
-      <div class="w-row">
-        <div class="w-col w-col-2 w-col-small-2 w-col-tiny-2"></div>
-        <div class="w-col w-col-8 w-col-small-8 w-col-tiny-8"><a href="index.html" class="logo-link w-inline-block w--current"><img src="images/beLogo.png" width="724"></a></div>
-        <div class="mobile-nav-col dropdown w-col w-col-2 w-col-small-2 w-col-tiny-2"><a href="#" data-ix="close-mobile-menu" class="close-button"><strong data-new-link="true">X</strong></a></div>
-      </div><a href="lessons/lessons-sitemap.html" class="dropdown-banner-link w-hidden-small w-inline-block"><img src="images/brighterBanner01.png" width="580" class="banner-img"></a>
-      <div class="mobile-menu-link-div"><a href="lessons/lessons-sitemap.html" class="mobile-nav-link">Lessons</a><a href="services/services-sitemap.html" class="mobile-nav-link">Services</a><a href="about-us/about-us-sitemap.html" class="mobile-nav-link">About Us</a><a href="calendar.html" class="mobile-nav-link">Calendar</a><a href="gift-certificates.html" class="mobile-nav-link">Gift Certificates</a><a href="visit-us.html" class="mobile-nav-link">Visit Us</a><a href="contact.html" class="mobile-nav-link">Contact Us</a></div>
+    <div class="section header w-hidden-medium w-hidden-small w-hidden-tiny">
+      <div class="header-container w-hidden-medium w-hidden-small w-hidden-tiny w-container">
+        <div class="header-section top">
+          <div class="header-row w-row">
+            <div class="w-col w-col-6">
+              <div class="header-title left">
+                <a href="gift-certificates.html" class="titling blue">Gift Certificates</a>
+                <a href="gift-certificates.html" class="titling sub">See Details</a>
+              </div>
+            </div>
+            <div class="w-col w-col-6">
+              <div class="header-title right">
+                <a href="contact.html" class="titling sub">Contact Us</a>
+                <a href="tel:(502) 893-8222" class="link small">(502) 893-8222</a>
+                <a href="tel:(502) 396-4207" class="link small">(502) 396-4207</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="header-title sub">Let&#x27;s Get You Dancing</div>
+        <a href="index.html" class="logo-link w-inline-block w--current">
+          <img src="images/beLogo.png" alt="Ballroom East Logo">
+        </a>
+        <div data-collapse="small" data-animation="default" data-duration="400" data-doc-height="1" class="navbar w-nav">
+          <div class="navbar-container w-container">
+            <div class="menu-button w-nav-button">
+              <div class="w-icon-nav-menu"></div>
+            </div>
+            <nav role="navigation" class="nav-menu w-clearfix w-nav-menu">
+              <div class="nav-menu-row _1st-half w-row">
+                <div class="w-col w-col-4">
+                  <div data-delay="0" data-ix="dropdown-on-hover" class="menu-dropdown w-dropdown">
+                    <div class="link menu w-dropdown-toggle">
+                      <div>Lessons</div>
+                    </div>
+                    <nav class="dropdown-list-wrapper w-dropdown-list">
+                      <div data-ix="dropdown-on-hover" class="dropdown-block">
+                        <div class="titling blue">Lessons</div>
+                        <div class="w-row">
+                          <div class="w-col w-col-3">
+                            <div class="dropdown-col">
+                              <div class="titling blue">Group Lessons</div><a href="lessons/beginner-lessons.html" class="link menu inmenu">Beginners</a><a href="lessons/intermediate-lessons.html" class="link menu inmenu">Intermediate</a><a href="lessons/advanced-lessons.html" class="link menu inmenu">Advanced</a></div>
+                          </div>
+                          <div class="w-col w-col-3">
+                            <div class="dropdown-col">
+                              <div class="titling blue">Private Group LEssons</div><a href="lessons/private-group.html" class="link menu inmenu">Private Group Lessons</a><a href="lessons/wedding-party.html" class="link menu inmenu">Wedding Party</a><a href="lessons/group-of-friends.html" class="link menu inmenu">Group of Friends</a></div>
+                          </div>
+                          <div class="w-col w-col-3">
+                            <div class="dropdown-col">
+                              <div class="titling blue">Private Lessons</div><a href="lessons/private-lessons.html" class="link menu inmenu">Private Lessons</a><a href="lessons/couples-lessons.html" class="link menu inmenu">Couples Lessons</a><a href="lessons/singles-lessons.html" class="link menu inmenu">Singles Lessons</a></div>
+                          </div>
+                          <div class="w-col w-col-3">
+                            <div class="dropdown-col">
+                              <div class="titling blue">Kids Lessons</div><a href="lessons/kids-group-class.html" class="link menu inmenu">Kids Group Class</a><a href="lessons/kids-private-lessons.html" class="link menu inmenu">Kids Private Lessons</a></div>
+                          </div>
+                        </div>
+                      </div>
+                    </nav>
+                  </div>
+                </div>
+                <div class="w-col w-col-4">
+                  <div data-delay="0" data-ix="dropdown-on-hover-2" class="menu-dropdown w-dropdown">
+                    <div class="link menu alt w-dropdown-toggle">
+                      <div>Services</div>
+                    </div>
+                    <nav class="dropdown-list-wrapper-2 w-dropdown-list">
+                      <div data-ix="dropdown-on-hover" class="dropdown-block _2">
+                        <div class="titling blue">Services</div>
+                        <div class="menu-dropdown-row w-row">
+                          <div class="menu-dropdown-col w-col w-col-6">
+                            <div class="dropdown-col">
+                              <div class="titling blue">What you can do</div><a href="services/proam.html" class="link menu inmenu">Pro/Am Competitions</a><a href="services/amam.html" class="link menu inmenu">Amateur Competitions</a><a href="services/showcases.html" class="link menu inmenu">Showcases</a><a href="services/social-dancing.html" class="link menu inmenu">Social Dances</a></div>
+                          </div>
+                          <div class="w-col w-col-6">
+                            <div class="dropdown-col">
+                              <div class="titling blue">What we can do</div><a href="services/out-of-studio-group-lessons.html" class="link menu inmenu">Out-of-Studio Group Classes</a><a href="services/out-of-town-coaching.html" class="link menu inmenu">Out-of-Town Coaching</a><a href="services/uoflballroom.html" class="link menu inmenu">University of Louisville Classes</a><a href="services/judging.html" class="link menu inmenu">Judging</a></div>
+                          </div>
+                        </div>
+                      </div>
+                    </nav>
+                  </div>
+                </div>
+                <div class="w-col w-col-4">
+                  <div data-delay="0" data-ix="dropdown-on-hover-3" class="menu-dropdown w-dropdown">
+                    <div class="link menu w-dropdown-toggle">
+                      <div>About Us</div>
+                    </div>
+                    <nav class="dropdown-list-wrapper-3 w-dropdown-list">
+                      <div data-ix="dropdown-on-hover" class="dropdown-block _3">
+                        <div class="titling blue">About US</div><a href="about-us/our-story.html" class="link menu inmenu">Our Story</a><a href="about-us/our-instructors.html" class="link menu inmenu">Our Instructors</a></div>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+              <div class="nav-menu-row _2nd-half w-row">
+                <div class="w-col w-col-3"><a href="calendar.html" class="link menu w-nav-link">Calendar</a></div>
+                <div class="menu-col w-col w-col-3"><a href="gift-certificates.html" class="link menu gift w-nav-link">Gift Certificates</a></div>
+                <div class="w-col w-col-3"><a href="visit-us.html" class="link menu w-nav-link">Visit Us</a></div>
+                <div class="w-col w-col-3"><a href="contact.html" class="link menu contact w-nav-link">Contact</a></div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,171 +1,88 @@
 <template>
   <section>
-    <div class="section header w-hidden-medium w-hidden-small w-hidden-tiny">
-    <div class="header-container w-hidden-medium w-hidden-small w-hidden-tiny w-container">
-      <div class="header-section top">
-        <div class="header-row w-row">
-          <div class="w-col w-col-6">
-            <div class="header-title left"><a href="gift-certificates.html" class="titling blue">Gift Certificates</a> <a href="gift-certificates.html" class="titling sub">See Details</a></div>
+    <div class="section w-hidden-small w-hidden-tiny">
+      <div class="w-container">
+        <div data-animation="slide" data-duration="500" data-infinite="1" data-delay="4000" data-autoplay="1" class="slider home w-slider">
+          <div class="w-slider-mask">
+            <div class="slide _1 w-slide"></div>
+            <div class="slide _2 w-slide"></div>
           </div>
-          <div class="w-col w-col-6">
-            <div class="header-title right"><a href="contact.html" class="titling sub">Contact Us</a>    <a href="tel:(502) 893-8222" class="link small">(502) 893-8222</a>    <a href="tel:(502) 396-4207" class="link small">(502) 396-4207</a></div>
+          <div class="w-slider-arrow-left">
+            <div class="w-icon-slider-left"></div>
           </div>
+          <div class="w-slider-arrow-right">
+            <div class="w-icon-slider-right"></div>
+          </div>
+          <div class="w-slider-nav w-round"></div>
         </div>
       </div>
-      <div class="header-title sub">Let&#x27;s Get You Dancing</div><a href="index.html" class="logo-link w-inline-block w--current"><img src="images/beLogo.png" alt="Ballroom East Logo"></a>
-      <div data-collapse="small" data-animation="default" data-duration="400" data-doc-height="1" class="navbar w-nav">
-        <div class="navbar-container w-container">
-          <div class="menu-button w-nav-button">
-            <div class="w-icon-nav-menu"></div>
-          </div>
-          <nav role="navigation" class="nav-menu w-clearfix w-nav-menu">
-            <div class="nav-menu-row _1st-half w-row">
-              <div class="w-col w-col-4">
-                <div data-delay="0" data-ix="dropdown-on-hover" class="menu-dropdown w-dropdown">
-                  <div class="link menu w-dropdown-toggle">
-                    <div>Lessons</div>
-                  </div>
-                  <nav class="dropdown-list-wrapper w-dropdown-list">
-                    <div data-ix="dropdown-on-hover" class="dropdown-block">
-                      <div class="titling blue">Lessons</div>
-                      <div class="w-row">
-                        <div class="w-col w-col-3">
-                          <div class="dropdown-col">
-                            <div class="titling blue">Group Lessons</div><a href="lessons/beginner-lessons.html" class="link menu inmenu">Beginners</a><a href="lessons/intermediate-lessons.html" class="link menu inmenu">Intermediate</a><a href="lessons/advanced-lessons.html" class="link menu inmenu">Advanced</a></div>
-                        </div>
-                        <div class="w-col w-col-3">
-                          <div class="dropdown-col">
-                            <div class="titling blue">Private Group LEssons</div><a href="lessons/private-group.html" class="link menu inmenu">Private Group Lessons</a><a href="lessons/wedding-party.html" class="link menu inmenu">Wedding Party</a><a href="lessons/group-of-friends.html" class="link menu inmenu">Group of Friends</a></div>
-                        </div>
-                        <div class="w-col w-col-3">
-                          <div class="dropdown-col">
-                            <div class="titling blue">Private Lessons</div><a href="lessons/private-lessons.html" class="link menu inmenu">Private Lessons</a><a href="lessons/couples-lessons.html" class="link menu inmenu">Couples Lessons</a><a href="lessons/singles-lessons.html" class="link menu inmenu">Singles Lessons</a></div>
-                        </div>
-                        <div class="w-col w-col-3">
-                          <div class="dropdown-col">
-                            <div class="titling blue">Kids Lessons</div><a href="lessons/kids-group-class.html" class="link menu inmenu">Kids Group Class</a><a href="lessons/kids-private-lessons.html" class="link menu inmenu">Kids Private Lessons</a></div>
-                        </div>
-                      </div>
-                    </div>
-                  </nav>
-                </div>
-              </div>
-              <div class="w-col w-col-4">
-                <div data-delay="0" data-ix="dropdown-on-hover-2" class="menu-dropdown w-dropdown">
-                  <div class="link menu alt w-dropdown-toggle">
-                    <div>Services</div>
-                  </div>
-                  <nav class="dropdown-list-wrapper-2 w-dropdown-list">
-                    <div data-ix="dropdown-on-hover" class="dropdown-block _2">
-                      <div class="titling blue">Services</div>
-                      <div class="menu-dropdown-row w-row">
-                        <div class="menu-dropdown-col w-col w-col-6">
-                          <div class="dropdown-col">
-                            <div class="titling blue">What you can do</div><a href="services/proam.html" class="link menu inmenu">Pro/Am Competitions</a><a href="services/amam.html" class="link menu inmenu">Amateur Competitions</a><a href="services/showcases.html" class="link menu inmenu">Showcases</a><a href="services/social-dancing.html" class="link menu inmenu">Social Dances</a></div>
-                        </div>
-                        <div class="w-col w-col-6">
-                          <div class="dropdown-col">
-                            <div class="titling blue">What we can do</div><a href="services/out-of-studio-group-lessons.html" class="link menu inmenu">Out-of-Studio Group Classes</a><a href="services/out-of-town-coaching.html" class="link menu inmenu">Out-of-Town Coaching</a><a href="services/uoflballroom.html" class="link menu inmenu">University of Louisville Classes</a><a href="services/judging.html" class="link menu inmenu">Judging</a></div>
-                        </div>
-                      </div>
-                    </div>
-                  </nav>
-                </div>
-              </div>
-              <div class="w-col w-col-4">
-                <div data-delay="0" data-ix="dropdown-on-hover-3" class="menu-dropdown w-dropdown">
-                  <div class="link menu w-dropdown-toggle">
-                    <div>About Us</div>
-                  </div>
-                  <nav class="dropdown-list-wrapper-3 w-dropdown-list">
-                    <div data-ix="dropdown-on-hover" class="dropdown-block _3">
-                      <div class="titling blue">About US</div><a href="about-us/our-story.html" class="link menu inmenu">Our Story</a><a href="about-us/our-instructors.html" class="link menu inmenu">Our Instructors</a></div>
-                  </nav>
-                </div>
-              </div>
+    </div>
+    <div class="section welcome content">
+      <div class="w-container"><img src="images/Banner07.png" class="w-hidden-main w-hidden-medium w-hidden-small w-hidden-tiny">
+        <div class="w-row">
+          <div class="w-hidden-small w-hidden-tiny w-col w-col-4">
+            <div class="vid-block">
+              <a href="#" class="lightbox w-inline-block w-lightbox">
+                <img src="images/video.png" alt="Preview image of intro video">
+                <script type="application/json" class="w-json">
+                {
+                  "items": [
+                    {
+                      "type": "video",
+                      "html": "<iframe class=\"embedly-embed\" src=\"https://cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2F326-pJbPQtM%3Fautoplay%3D1%26feature%3Doembed&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D326-pJbPQtM%26feature%3Dyoutu.be&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2F326-pJbPQtM%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&autoplay=1&type=text%2Fhtml&schema=youtube\" width=\"940\" height=\"528\" scrolling=\"no\" frameborder=\"0\" allowfullscreen></iframe>",
+                      "thumbnailUrl": "https://i.embed.ly/1/image?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F326-pJbPQtM%2Fhqdefault.jpg&key=866738f8f18b4394a18e371517a66ae0",
+                      "width": 940,
+                      "height": 528,
+                      "originalUrl": "https://youtu.be/326-pJbPQtM",
+                      "url": "http://www.youtube.com/watch?v=326-pJbPQtM"
+                    }
+                  ]
+                }
+                </script>
+              </a>
             </div>
-            <div class="nav-menu-row _2nd-half w-row">
-              <div class="w-col w-col-3"><a href="calendar.html" class="link menu w-nav-link">Calendar</a></div>
-              <div class="menu-col w-col w-col-3"><a href="gift-certificates.html" class="link menu gift w-nav-link">Gift Certificates</a></div>
-              <div class="w-col w-col-3"><a href="visit-us.html" class="link menu w-nav-link">Visit Us</a></div>
-              <div class="w-col w-col-3"><a href="contact.html" class="link menu contact w-nav-link">Contact</a></div>
+          </div>
+          <div class="col w-col w-col-8">
+            <div class="content-block welcome">
+              <div class="lesson-type-title large w-hidden-small w-hidden-tiny">Let&#x27;s get you Dancing!</div>
+              <div class="lesson-type-title large w-hidden-main w-hidden-medium">Welcome to ballroom East</div>
+              <p class="paragraph _1 w-hidden-small w-hidden-tiny">
+                Whether you want to learn Salsa, Swing, Tango or Waltz, our dance instructors will ensure the pace is comfortable and lessons mentally digestible. If you have always dreamed of becoming a dance maestro then Ballroom East is the dance academy for you! Regardless of age or experience, Ballroom East has the savvy instructors and dance knowledge to make sure your learning experience is comfortable and effective.
+              </p>
             </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="section w-hidden-small w-hidden-tiny">
-    <div class="w-container">
-      <div data-animation="slide" data-duration="500" data-infinite="1" data-delay="4000" data-autoplay="1" class="slider home w-slider">
-        <div class="w-slider-mask">
-          <div class="slide _1 w-slide"></div>
-          <div class="slide _2 w-slide"></div>
-        </div>
-        <div class="w-slider-arrow-left">
-          <div class="w-icon-slider-left"></div>
-        </div>
-        <div class="w-slider-arrow-right">
-          <div class="w-icon-slider-right"></div>
-        </div>
-        <div class="w-slider-nav w-round"></div>
-      </div>
-    </div>
-  </div>
-  <div class="section welcome content">
-    <div class="w-container"><img src="images/Banner07.png" class="w-hidden-main w-hidden-medium w-hidden-small w-hidden-tiny">
-      <div class="w-row">
-        <div class="w-hidden-small w-hidden-tiny w-col w-col-4">
-          <div class="vid-block"><a href="#" class="lightbox w-inline-block w-lightbox"><img src="images/video.png" alt="Preview image of intro video"><script type="application/json" class="w-json">{
-  "items": [
-    {
-      "type": "video",
-      "html": "<iframe class=\"embedly-embed\" src=\"https://cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2F326-pJbPQtM%3Fautoplay%3D1%26feature%3Doembed&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D326-pJbPQtM%26feature%3Dyoutu.be&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2F326-pJbPQtM%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&autoplay=1&type=text%2Fhtml&schema=youtube\" width=\"940\" height=\"528\" scrolling=\"no\" frameborder=\"0\" allowfullscreen></iframe>",
-      "thumbnailUrl": "https://i.embed.ly/1/image?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F326-pJbPQtM%2Fhqdefault.jpg&key=866738f8f18b4394a18e371517a66ae0",
-      "width": 940,
-      "height": 528,
-      "originalUrl": "https://youtu.be/326-pJbPQtM",
-      "url": "http://www.youtube.com/watch?v=326-pJbPQtM"
-    }
-  ]
-}</script></a></div>
-        </div>
-        <div class="col w-col w-col-8">
-          <div class="content-block welcome">
-            <div class="lesson-type-title large w-hidden-small w-hidden-tiny">Let&#x27;s get you Dancing!</div>
-            <div class="lesson-type-title large w-hidden-main w-hidden-medium">Welcome to ballroom East</div>
-            <p class="paragraph _1 w-hidden-small w-hidden-tiny">Whether you want to learn Salsa, Swing, Tango or Waltz, our dance instructors will ensure the pace is comfortable and lessons mentally digestible. If you have always dreamed of becoming a dance maestro then Ballroom East is the dance academy for you! Regardless of age or experience, Ballroom East has the savvy instructors and dance knowledge to make sure your learning experience is comfortable and effective.</p>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="section content">
-    <div class="w-container">
-      <div class="lesson-row w-row">
-        <div class="lesson-col w-col w-col-4 w-col-small-4">
-          <a href="lessons/lessons-sitemap.html" class="lesson-type-block w-inline-block">
-            <div class="lesson-type-title">Adult Lessons</div><img src="images/brighteradults-image-new1.png" alt="Dance Instruction" class="col-img">
-            <div class="lesson-type-text">Private/Group Classes available all week</div>
-            <div class="titling blue links-bolded">Get started &gt;&gt;</div>
-          </a>
-        </div>
-        <div class="lesson-col w-col w-col-4 w-col-small-4">
-          <a href="lessons/kids-group-class.html" class="lesson-type-block w-inline-block">
-            <div class="lesson-type-title">Kids Lessons</div><img src="images/websitekidsphoto_new.png" alt="Dance Instruction" class="col-img">
-            <div class="lesson-type-text">Fun lessons for kids</div>
-            <div class="titling blue links-bolded">Find out more &gt;&gt;</div>
-          </a>
-        </div>
-        <div class="lesson-col w-col w-col-4 w-col-small-4">
-          <a href="lessons/wedding-party.html" class="lesson-type-block w-inline-block">
-            <div class="lesson-type-title">Wedding Lessons</div><img src="images/Wedding.png" alt="Dance Instruction" class="col-img">
-            <div class="lesson-type-text">Make your wedding dance incredibly special</div>
-            <div class="titling blue links-bolded">See Options &gt;&gt;</div>
-          </a>
+    <div class="section content">
+      <div class="w-container">
+        <div class="lesson-row w-row">
+          <div class="lesson-col w-col w-col-4 w-col-small-4">
+            <a href="lessons/lessons-sitemap.html" class="lesson-type-block w-inline-block">
+              <div class="lesson-type-title">Adult Lessons</div>
+              <img src="images/brighteradults-image-new1.png" alt="Dance Instruction" class="col-img">
+              <div class="lesson-type-text">Private/Group Classes available all week</div>
+              <div class="titling blue links-bolded">Get started &gt;&gt;</div>
+            </a>
+          </div>
+          <div class="lesson-col w-col w-col-4 w-col-small-4">
+            <a href="lessons/kids-group-class.html" class="lesson-type-block w-inline-block">
+              <div class="lesson-type-title">Kids Lessons</div>
+              <img src="images/websitekidsphoto_new.png" alt="Dance Instruction" class="col-img">
+              <div class="lesson-type-text">Fun lessons for kids</div>
+              <div class="titling blue links-bolded">Find out more &gt;&gt;</div>
+            </a>
+          </div>
+          <div class="lesson-col w-col w-col-4 w-col-small-4">
+            <a href="lessons/wedding-party.html" class="lesson-type-block w-inline-block">
+              <div class="lesson-type-title">Wedding Lessons</div>
+              <img src="images/Wedding.png" alt="Dance Instruction" class="col-img">
+              <div class="lesson-type-text">Make your wedding dance incredibly special</div>
+              <div class="titling blue links-bolded">See Options &gt;&gt;</div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </section>
 </template>
