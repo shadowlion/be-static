fededab8fb44b9aaec9e02af24b5e37f1a4beb30
Justin <chiou.kai@gmail.com>
2018-06-14 22:32:20 -0400

    add the Visit Us page

---
+++ pages/visit.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="section content">
+      <div class="w-container">
+        <div class="w-row">
+          <div class="visit-banner-col w-col w-col-4 w-col-stack">
+            <div class="visit-text-block">
+              <div class="visit-banner-content-block">
+                <div class="titling white">Lessons</div>
+                <div class="titling white small">at Ballroom East</div>
+                <p class="paragraph white">Shopping for dance Lessons is easy online with our Gift Certificate shopping cart.<br>We can’t wait to meet you!</p>
+              </div>
+            </div>
+          </div>
+          <div class="visit-banner-col w-col w-col-8 w-col-stack"><img src="images/BallroomEast-buildingenlarged.jpg" alt="Ballroom East Studio Building " srcset="images/BallroomEast-buildingenlarged-p-500x222.jpeg 500w, images/BallroomEast-buildingenlarged.jpg 800w" sizes="(max-width: 479px) 90vw, (max-width: 767px) 94vw, (max-width: 991px) 96vw, 639.984375px"></div>
+        </div>
+      </div>
+    </div>
+    <div class="visit-cta-section">
+      <div class="visit-cta-container w-container">
+        <div class="visit-cta-header-block">
+          <div class="titling blue large bannered">Studio</div>
+        </div>
+        <div>We invite you to stop by and visit us.</div>
+        <div class="visit-text largest">Louisville, KY - Dupont Road</div>
+        <div class="visit-text verd">906 Dupont Rd. <br>(Behind WW Cousins Restaurant) <br><a target="_blank" href="https://maps.google.com/maps?hl=en&amp;q=906+Dupont+Road,+Louisville,+KY+40206&amp;ie=UTF8&amp;hq=&amp;hnear=906+Dupont+Rd,+Louisville,+Kentucky+40207&amp;gl=us&amp;t=m&amp;ll=38.244517,-85.638771&amp;spn=0.030334,0.050554&amp;z=14&amp;iwloc=A&amp;source=embed" class="link menu blue">Directions &gt;</a></div>
+        <div class="w-row">
+          <div class="w-col w-col-4">
+            <div class="visit-text verd"><a href="tel:502-893-8222" class="link small">502-893-8222</a></div>
+          </div>
+          <div class="w-col w-col-4">
+            <div class="visit-text verd"><a target="_blank" href="https://www.facebook.com/ballroomeast" class="link menu blue">FACEBOOK </a></div>
+          </div>
+          <div class="w-col w-col-4">
+            <div class="visit-text verd"><a target="_blank" href="https://twitter.com/ballroomeast" class="link menu blue">TWITTER</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section maps">
+      <div class="w-container">
+        <div data-widget-latlng="38.234835,-85.63933" data-widget-style="roadmap" data-widget-zoom="12" class="w-widget w-widget-map"></div>
+      </div>
+      <div class="livetour-container w-container">
+        <div class="w-hidden-small w-hidden-tiny w-embed w-iframe"><iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1469049477712!6m8!1m7!1sK0JjtkvE-gcAAAQrAWrFDQ!2m2!1d38.23477415907963!2d-85.63935713529884!3f263.25331999046955!4f-3.774728743151769!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe></div>
+        <div class="w-hidden-main w-hidden-medium w-embed w-iframe"><iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1469049477712!6m8!1m7!1sK0JjtkvE-gcAAAQrAWrFDQ!2m2!1d38.23477415907963!2d-85.63935713529884!3f263.25331999046955!4f-3.774728743151769!5f0.7820865974627469" width="250" height="320" frameborder="0" style="border:0" allowfullscreen=""></iframe></div>
+      </div>
+    </div>
+  </div>
+</template>
